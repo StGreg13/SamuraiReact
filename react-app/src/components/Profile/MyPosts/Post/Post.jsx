c5b5bed82e20902ec377b1547bed69ab40cd2345
StGreg13 <gregory.stepanov@live.ru>
2022-10-06 17:04:26 +0400

    Main separate and update components

---
+++ react-app/src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,17 @@
+import s from "./Post.module.css";
+
+const Post = (props) => {
+  return (
+    <div className={s.item}>
+      <div className={s.ava}>
+        <img src="https://bit.ly/3SJeKCs" alt="avatar" />
+        {props.message}
+        <div>
+          <span>like</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
--- react-app/src/components/Profile/Profile.jsx
+++ react-app/src/components/Profile/Profile.jsx
@@ -1,18 +1,14 @@
+import s from "./Profile.module.css";
+import MyPosts from "./MyPosts/MyPosts";
+
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350" />
       </div>
       <div>ava + description</div>
-      <div>
-        My posts
-        <div>New post</div>
-        <div>
-          <div>post 1</div>
-          <div>post 2</div>
-        </div>
-      </div>
+      <MyPosts />
     </div>
   );
 };
