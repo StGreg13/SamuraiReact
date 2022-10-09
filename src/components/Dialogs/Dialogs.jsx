22e77766d8a58a7dc7e3d7fd727b5e489df03100
StGreg13 <gregory.stepanov@live.ru>
2022-10-09 21:15:16 +0400

    add dialog item

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,36 @@
 import s from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    return (
+        <div className={s.dialog + " " + s.active}>
+            <NavLink to={"dialogs/" + props.id}>
+                {props.name}
+            </NavLink>
+        </div>
+    )
+}
 
 const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <div className={s.dialog + " " + s.active}>Dimych</div>
-        <div className={s.dialog}>Andrey</div>
-        <div className={s.dialog}>Sveta</div>
-        <div className={s.dialog}>Nikita</div>
+            <DialogItem name="Dimych" id="1"/>
+        <div className={s.dialog}>
+            <NavLink  to="dialogs/2">
+                Andrey
+            </NavLink>
+        </div>
+        <div className={s.dialog}>
+            <NavLink  to="dialogs/3">
+                Sveta
+            </NavLink>
+        </div>
+        <div className={s.dialog}>
+            <NavLink  to="dialogs/4">
+                Nikita
+            </NavLink>
+        </div>
       </div>
       <div className={s.messages}>
         <div className={s.message}>hi</div>
